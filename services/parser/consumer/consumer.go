b90bbe1b5b57a09134407f01d7e2160b54dcf62f
Marie20767 <marie20767@gmail.com>
2026-04-08 15:32:11 +1000

    Implemented Consume method and extracted HTTPError into /shared httperr package

--- services/parser/consumer/consumer.go
+++ services/parser/consumer/consumer.go
@@ -2,10 +2,13 @@ package consumer
 
 import (
 	"context"
+	"errors"
 	"log/slog"
+	"sync"
 	"time"
 
 	"github.com/marie20767/web-crawler/services/parser/config"
+	"github.com/marie20767/web-crawler/shared/httperr"
 	"github.com/marie20767/web-crawler/shared/objstorage"
 	"github.com/segmentio/kafka-go"
 )
@@ -48,6 +51,85 @@ func New(ctx context.Context, kafkaCfg *config.Kafka, awsCfg *config.AWS) (*Cons
 }
 
 func (c *Consumer) Consume() error {
+	jobs := make(chan kafka.Message, workerCount)
+	var wg sync.WaitGroup
+
+	for range workerCount {
+		wg.Go(func() {
+			for job := range jobs {
+				slog.Info("processing message", slog.String("id", string(job.Key)))
+				if err := c.processMessage(&job); err != nil {
+					slog.Error("process message", slog.Any("error", err))
+					var hErr *httperr.Err
+					errStatusCode := 0
+					if errors.As(err, &hErr) {
+						errStatusCode = hErr.StatusCode
+					}
+
+					// TODO: publish to DLQ
+					slog.Info("code", errStatusCode)
+					// c.producer.PublishDLQ(&job, errStatusCode)
+				}
+
+				if err := c.reader.CommitMessages(context.WithoutCancel(c.ctx), job); err != nil {
+					slog.Error("commit message offset", slog.Any("error", err))
+				}
+			}
+		})
+	}
+
+	defer wg.Wait()
+	defer close(jobs)
+
+	for {
+		readCtx, cancel := context.WithTimeout(c.ctx, kafkaTimeout)
+		msg, err := c.reader.FetchMessage(readCtx)
+		cancel()
+
+		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				timer := time.NewTimer(kafkaPollInterval)
+
+				slog.Info("no messages available, waiting to poll again", slog.Int64("interval_seconds", int64(kafkaPollInterval.Seconds())))
+				select {
+				case <-c.ctx.Done():
+					timer.Stop()
+					slog.Info("context cancelled")
+					return nil
+				case <-timer.C:
+					continue
+				}
+			}
+
+			if errors.Is(err, context.Canceled) {
+				slog.Info("context cancelled")
+				return nil
+			}
+
+			slog.Error("consume from topic", slog.Any("error", err))
+			return err
+		}
+
+		select {
+		case jobs <- msg:
+		case <-c.ctx.Done():
+			slog.Warn("context cancelled, dropping unqueued message", slog.String("url", string(msg.Key)))
+			return nil
+		}
+	}
+}
+
+func (c *Consumer) processMessage(msg *kafka.Message) error {
+	slog.Info("msg", msg)
+	// TODO: check url exists in DB, if yes commit offset, if no run logic below
+
+	// TODO: fetch raw HTML from s3
+
+	// TODO:
+	// extract text & new urls
+	// save text to s3
+	// produce to init topic
+
 	return nil
 }
 
